9995dce00fa69a9224e6ac5a4cbacf8baaee674a
Rafael <rafael.desouzaferreira@student.griffith.ie>
2021-05-04 18:44:20 +0100

    Creating Chatbot

--- chatbot/Gui_Chatbot.py
+++ chatbot/Gui_Chatbot.py
@@ -1,1 +1,64 @@
 # This file is where we will build a graphical user interface to chat with our trained chatbot.
+
+# Step 5. Interacting With the Chatbot
+# It will be used Tkinter module to build the structure of the desktop application
+
+import nltk
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
+import pickle
+import numpy as np
+
+from keras.models import load_model
+model = load_model('chatbot_model.h5')
+import json
+import random
+intents = json.loads(open('intents.json').read())
+words = pickle.loads(open('words.pkl', 'rb'))
+classes = pickle.load(open('classes.pkl', 'rb'))
+
+def clean_up_sentece(setence):
+    # Tokenize the pattern - splitting words into an array
+    sentence_words = nltk.word_tokenize(setence)
+    # Stemming every word - reducing to base form
+    sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
+    return sentence_words
+
+# Return bag of words array: 0 or 1 for words that exist in sentence
+def bag_of_words(sentence, words, show_details=True):
+    # Tokenizing patterns
+    sentence_words = clean_up_sentece(sentence)
+    # Bag of words - Vocabulary Matrix
+    bag = [0]*len(words)
+    for s in sentence_words:
+        for i, word in enumerate(words):
+            if word == s:
+                # Assign 1 if current word is in the vocabulary position
+                bag[i] = 1
+                if show_details:
+                    print('Found in bag: %s' % word)
+    return (np.array(bag))
+
+def predict_class(sentence):
+    # Filter below threshold predictions
+    p = bag_of_words(sentence, words, show_details=False)
+    res = model.predict(np.array([p]))[0]
+    ERROR_THRESHOLD = 0.25
+    results = [[i,r] for i,r in enumerate(res) if r > ERROR_THRESHOLD]
+    # Sorting  strength probability
+    results.sort(key=lambda x:x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
+    return return_list
+
+def getResponse(ints, intents_json):
+    tag = ints[0]['intent']
+    list_of_intents = intents_json['intents']
+    for i in list_of_intents:
+        if(i['tag'] == tag):
+            result = random.choice(i['responses'])
+            break
+    return result
+
+# Creating Tkinter GUI
