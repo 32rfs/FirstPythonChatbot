b4f7a61a3ea71729aa4235a2ee4e4f2555edde7a
Rafael <rafael.desouzaferreira@student.griffith.ie>
2021-05-07 20:23:40 +0100

    Finishing GUI

--- chatbot/Gui_Chatbot.py
+++ chatbot/Gui_Chatbot.py
@@ -14,7 +14,7 @@ model = load_model('chatbot_model.h5')
 import json
 import random
 intents = json.loads(open('intents.json').read())
-words = pickle.loads(open('words.pkl', 'rb'))
+words = pickle.load(open('words.pkl', 'rb'))
 classes = pickle.load(open('classes.pkl', 'rb'))
 
 def clean_up_sentece(setence):
@@ -41,12 +41,12 @@ def bag_of_words(sentence, words, show_details=True):
 
 def predict_class(sentence):
     # Filter below threshold predictions
-    p = bag_of_words(sentence, words, show_details=False)
+    p = bag_of_words(sentence, words,show_details=False)
     res = model.predict(np.array([p]))[0]
     ERROR_THRESHOLD = 0.25
-    results = [[i,r] for i,r in enumerate(res) if r > ERROR_THRESHOLD]
+    results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
     # Sorting  strength probability
-    results.sort(key=lambda x:x[1], reverse=True)
+    results.sort(key=lambda x: x[1], reverse=True)
     return_list = []
     for r in results:
         return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
@@ -62,3 +62,50 @@ def getResponse(ints, intents_json):
     return result
 
 # Creating Tkinter GUI
+import tkinter
+from tkinter import *
+
+def send():
+    msg = EntryBox.get("1.0",'end-1c').strip()
+    EntryBox.delete("0.0", END)
+
+    if msg != '':
+        ChatBox.config(state=NORMAL)
+        ChatBox.insert(END, "You: " + msg + '\n\n')
+        ChatBox.config(foreground="#446665", font=("Verdana", 12))
+
+        ints = predict_class(msg)
+        res = getResponse(ints, intents)
+
+        ChatBox.insert(END, "Bot: " + res + '\n\n')
+        ChatBox.config(state=DISABLED)
+        ChatBox.yview(END)
+
+root = Tk()
+root.title("Chatbot")
+root.geometry("400x500")
+root.resizable(width=FALSE, height=FALSE)
+
+# Creating Chatbox Window
+ChatBox = Text(root, bd=0, bg="white", height="8", width="50", font="Arial")
+ChatBox.config(state=DISABLED)
+
+# Binding Scrollbar to the Chat Window
+scrollbar = Scrollbar(root, command=ChatBox.yview, cursor="heart")
+ChatBox['yscrollcommand'] = scrollbar.set
+
+# Creating Button to send message
+SendButton = Button(root, font=("Verdana", 12, 'bold'), text="Send", width="12", height=5,
+                    bd=0, bg="#f9a602", activebackground="#3c9d9b", fg='#000000', command=send)
+
+# Creating the box to enter messages
+EntryBox = Text(root, bd=0, bg="white", width="29", height="5", font="Arial")
+# EntryBox.bind("<Return>", send)
+
+# Placing all elements on the screen
+scrollbar.place(x=376, y=6, height=386)
+ChatBox.place(x=6,y=6, height=386, width=370)
+EntryBox.place(x=128, y=401, height=90, width=265)
+SendButton.place(x=6, y=401, height=90)
+
+root.mainloop()
